06930c0ade539884e69841cf9a9a8bd3ab95f455
Ali Abdallah <94802761+ali20021973@users.noreply.github.com>
2022-09-23 13:12:17 +0200

    a

---
+++ includes.php
@@ -0,0 +1,16 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/Encryption.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/options.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/optionvalues.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/orderdetails.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/orders.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/product.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/productcategory.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/productcategoryoption.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/user.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/usertype.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/data_base_ali.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/GitHub/traning-Project/models/Buy_now.php";
+
+
+
